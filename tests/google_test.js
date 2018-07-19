c8c6cc96a1e02d33ccf784d9d913e37093df839e
Praveen Prasannan <praveen.prasann@gmail.com>
2018-07-19 16:27:31 +1000

    Edited test info

--- tests/google_test.js
+++ tests/google_test.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import HomePage from '../pages/HomePage';
 
 describe('Homepage test', () => {
-  it('should open the homepage with the correct title', () => {
+  it('should open the Google homepage with the expected title', () => {
     HomePage.open();
 
     expect(HomePage.title()).to.equal('Google');
