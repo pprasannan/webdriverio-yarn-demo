c407f24840bc759835a2fe7b6576a8dd43a286d9
Praveen Prasannan <praveen.prasann@gmail.com>
2018-07-19 17:04:32 +1000

    Added a second test

--- tests/google_test.js
+++ tests/google_test.js
@@ -1,10 +1,15 @@
 import { expect } from 'chai';
 import HomePage from '../pages/HomePage';
 
-describe('Homepage test', () => {
+describe('Google Tests', () => {
   it('should open the Google homepage with the expected title', () => {
     HomePage.open();
-
     expect(HomePage.title()).to.equal('Google');
+  });
+
+  it('should do a search to get results', () => {
+
+    HomePage.doASearch('sydney');
+    expect(HomePage.title()).to.equal('sydney - Google Search');
   });
 });
